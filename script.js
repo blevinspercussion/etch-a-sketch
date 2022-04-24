56a4d7c65d806a9566a9a15b8f47dc1c7db786de
blevinspercussion <blevinspercussion@gmail.com>
2022-04-24 13:14:10 -0500

    draw grid to the screen using divs instead of canvas

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+const container = document.querySelector('.container');
 
 function drawSquares() {
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            ctx.beginPath();
-            ctx.rect(10 + (i * 50), 10 + (j * 50), 50, 50);
-            ctx.stroke();
+    for (i = 0; i < 16; i++) {
+        for (j = 0; j < 16; j++) {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('cell');
+            container.appendChild(newDiv);
         }
     }
 }
 
+
 drawSquares();
+
+
+
+
+
+// const canvas = document.querySelector('canvas');
+// let ctx = canvas.getContext('2d');
+
+// function drawSquares() {
+//     for (i = 0; i < 16; i++) {
+//         for (j = 0; j < 16; j++) {
+//             ctx.beginPath();
+//             ctx.rect(10 + (i * 50), 10 + (j * 50), 50, 50);
+//             ctx.stroke();
+//         }
+//     }
+// }
+
+// drawSquares();
