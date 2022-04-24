3ddf77be72cb869ac194350541ab1b154f0341e9
blevinspercussion <blevinspercussion@gmail.com>
2022-04-24 13:49:00 -0500

    added ability to draw and erase squares

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const container = document.querySelector('.container');
 
+
+
 function drawSquares() {
     for (i = 0; i < 16; i++) {
         for (j = 0; j < 16; j++) {
             let newDiv = document.createElement('div');
             newDiv.classList.add('cell');
+            newDiv.classList.add('cell' + i + j);
             container.appendChild(newDiv);
         }
     }
-}
 
+}
 
 drawSquares();
 
+const cells = document.querySelectorAll('.cell');
 
-
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add('hilight');
+    });
+    cell.addEventListener('click', () => {
+        cell.classList.remove('hilight');
+    });
+});
 
 
 // const canvas = document.querySelector('canvas');
